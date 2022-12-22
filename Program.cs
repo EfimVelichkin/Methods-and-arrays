a64ba1573b68ee0c1f8db614d4fd1df940a5c8ee
EfimVelichkin <120294162+EfimVelichkin@users.noreply.github.com>
2022-12-22 17:50:55 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
-﻿int number = new Random().Next(10, 1000);
+﻿int number = new Random().Next(100, 1000);
 Console.WriteLine(number);
-int number1 = number % 10 / 1;
-if (number > 100) {
-    Console.WriteLine(number1);
-}
-else {
-    Console.WriteLine("Нет третьей цифры");
-}
+string new_number = "";
+new_number += number % 100 / 10;
+Console.WriteLine(new_number);
