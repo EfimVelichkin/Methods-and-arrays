6dcec57f1b913a073b76819d9a4ee2d3d3d0a8b0
EfimVelichkin <120294162+EfimVelichkin@users.noreply.github.com>
2022-12-22 17:54:42 +0700

    Example_13

---
+++ Example 13.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(10, 1000);
+Console.WriteLine(number);
+int number1 = number % 10 / 1;
+if (number > 100) {
+    Console.WriteLine(number1);
+}
+else {
+    Console.WriteLine("Нет третьей цифры");
+}
