f1123fec66c045106bad3f8b7e6465ebe165d013
EfimVelichkin <120294162+EfimVelichkin@users.noreply.github.com>
2022-12-30 13:14:38 +0700

    Example 19

---
+++ Example 19.cs
@@ -0,0 +1,20 @@
+﻿        int i, remainder = 0, newnum = 0;
+        Console.Write("Введите число: ");
+        int uinput = Convert.ToInt32((Console.ReadLine()));
+        for (i = uinput; i > 0; i = (i / 10))
+        {
+            remainder = i % 10;
+            newnum = (newnum*10) + remainder;
+
+        }
+
+
+        if (newnum == uinput)
+        {
+            Console.WriteLine("Число {0} палиндром", uinput);
+        }
+        else
+        {
+            Console.WriteLine(" Число не является палиндромом");
+        }
+        Console.ReadKey();
